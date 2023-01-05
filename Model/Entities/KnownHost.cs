8310bcaa93e12bacda25c507e8f06c0e880c6ebe
notrich22 <78218928+notrich22@users.noreply.github.com>
2023-01-05 12:10:25 +0200

    Delete Model directory

--- Model/Entities/KnownHost.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JSONAmveraAPIApp.Model.Entities
-{
-    public class KnownHost
-    {
-        public int Id { get; set; }
-        public string UserAgent { get; set; }
-        public string IP { get; set; }
-    }
-}
