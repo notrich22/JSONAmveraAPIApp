71b708409accc75ea3e96f583e8321306f25d6ca
notrich22 <notrich22@mail.ru>
2023-01-10 17:44:52 +0100

    Request updated

---
+++ JsonAmveraConvertationApiApp/Migrations/20230110164341_mig2343.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JSONAmveraAPIApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2343 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastUpdateTime",
+                table: "Requests",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastUpdateTime",
+                table: "Requests",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
--- JsonAmveraConvertationApiApp/Model/Entities/Request.cs
+++ JsonAmveraConvertationApiApp/Model/Entities/Request.cs
@@ -6,5 +6,7 @@
         public KnownHost KnownHost { get; set; }
         public bool isHttps { get; set; }
         public string Path { get; set; }
+        public string Time { get; set; }
+        public string? LastUpdateTime { get; set; }
     }
 }
